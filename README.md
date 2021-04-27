72383c61fa7eb757367be93edefea445c34e0df4
junui <wnsspdl@naver.com>
2021-04-28 00:00:35 +0900

    rss피드로 최근게시물 목록 불러오기

---
+++ README.md
@@ -0,0 +1,7 @@
+
+### ✍ Recent blog posts 
+[[프로그래머스] 디스크 컨트롤러 풀이](https://coconutstd.github.io/posts/%EB%94%94%EC%8A%A4%ED%81%AC%EC%BB%A8%ED%8A%B8%EB%A1%A4%EB%9F%AC/) <br>
+[크루스칼 알고리즘](https://coconutstd.github.io/posts/%ED%81%AC%EB%A3%A8%EC%8A%A4%EC%B9%BC%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98/) <br>
+[디자인패턴을 이해하기 위한 기초, 관계 이해](https://coconutstd.github.io/posts/%EB%94%94%EC%9E%90%EC%9D%B8%ED%8C%A8%ED%84%B4-UML-%EA%B4%80%EA%B3%84%EC%9D%B4%ED%95%B4/) <br>
+[[책 후기]스프링부트와 AWS로 혼자 구현하는 웹 서비스를 읽고나서](https://coconutstd.github.io/posts/%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8%EC%99%80-aws%EB%A1%9C-%ED%98%BC%EC%9E%90-%EA%B5%AC%ED%98%84%ED%95%98%EB%8A%94-%EC%9B%B9%EC%84%9C%EB%B9%84%EC%8A%A4/) <br>
+[자바스크립트로 리팩터링 하기, 코드 테스트 환경 구축](https://coconutstd.github.io/posts/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-%EB%A6%AC%ED%8C%A9%ED%84%B0%EB%A7%811/) <br>
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import feedparser, datetime, ssl
 
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+if hasattr(ssl, '_create_unverified_context'):
+    ssl._create_default_https_context = ssl._create_unverified_context
+blog_rss_uri = "https://coconutstd.github.io/feed.xml"
+feed = feedparser.parse(blog_rss_uri)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+markdown_text = """
+### ✍ Recent blog posts 
+"""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+j = 0
+for i in feed['entries']:
+    j += 1
+    if j > 5:
+        break
+    else:
+        dt = i['published']
+        markdown_text += f"[{i['title']}]({i['link']}) <br>\n"
+        print(i['link'], i['title'])
+
+f = open("README.md", mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
